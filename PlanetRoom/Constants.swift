30bfd6186234cb0e3132af074e96c1018e9abdb2
Emmanuel Cuevas <emmanuelcu@gmail.com>
2021-02-09 18:47:56 -0600

    First commit. This version is loading the image of the planet in the sphere. Also, some improvements in the UI were made, due the buttons of Next and Prev are graphically but don't have any functionality.

---
+++ PlanetRoom/Constants.swift
@@ -0,0 +1,22 @@
+//
+//  Constants.swift
+//  PlanetRoom
+//
+//  Created by Emmanuel Cuevas on 09/02/21.
+//
+
+import Foundation
+
+struct K {
+    
+    struct Planets {
+        static let Mercury = "art.scnassets/8k_mercury.jpg"
+        static let Venus = "art.scnassets/8k_venus.jpg"
+        static let Earth = "art.scnassets/8k_earth.jpg"
+        static let Mars = "art.scnassets/8k_mars.jpg"
+        static let Jupiter = "art.scnassets/8k_jupiter.jpg"
+        static let Saturn = "art.scnassets/8k_saturn.jpg"
+        static let Uranus = "art.scnassets/2k_uranus.jpg"
+        static let Neptune = "art.scnassets/2k_neptune.jpg"
+    }
+}
